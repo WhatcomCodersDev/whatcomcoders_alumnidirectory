c1c59643ac3226424400142cecd892c4303de401
hanselguzman-soto <soto26938@gmail.com>
2023-11-04 17:16:06 -0700

    attempt two

--- backend/email_script.py
+++ backend/email_script.py
@@ -25,7 +25,7 @@ data = {
       "value": "Hey Juan, \n \n This email was sent by using the SendGrid API. I am using Cloudflare to redirect this custom email through our domain. Send me an email or message when you have time to get set up. I have this almost set up. \n\n\n Thank you,\n Hansel"
     }
   ]
-}
+} 
 response = sg.client.mail.send.post(request_body=data)
 print(response.status_code)
 print(response.body)
