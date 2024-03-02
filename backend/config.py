972dd351e965bd332c0f278b35af57a0fb4acf28
hanselguzman-soto <soto26938@gmail.com>
2024-03-01 22:26:20 -0800

    all

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
