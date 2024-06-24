169a532d06786ad825cbb32d63e35de0eede3a1d
MinieMoe <minnguyen7.2001@gmail.com>
2024-06-24 12:06:05 -0700

    add feature section - no design

--- frontend/src/views/landing/index.js
+++ frontend/src/views/landing/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Button,
   Container,
@@ -48,264 +48,198 @@ const FeatureIcon = styled(Box)`
   background-repeat: no-repeat;
 `;
 
-// const LandingView = () => {
-//   return (
-//     <Container>
-//       <HeroImage>
-//         <Grid
-//           container
-//           spacing={2}
-//           alignItems="center"
-//           justifyContent="center"
-//           style={{ height: "100%" }}
-//         >
-//           <Grid item xs={12} md={6}>
-//             <Typography variant="h2" gutterBottom color="primary.main">
-//               Connect With WWU Tech Alumni
-//             </Typography>
-//             <Typography variant="h5" marginBottom={3}>
-//               Discover, Engage & Grow Together
-//             </Typography>
-//             <Box display="flex" flexDirection="row" alignItems="center" gap={2}>
-//               <Button
-//                 href={"/people"}
-//                 variant="contained"
-//                 size="large"
-//                 color="primary"
-//                 sx={{ borderRadius: 28 }} // Making the button round
-//               >
-//                 Meet People
-//               </Button>
-//               <Button
-//                 component="a"
-//                 href="https://discord.gg/r6ShrR73Jx"
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//                 variant="contained"
-//                 size="large"
-//                 sx={{
-//                   backgroundColor: "#ff9966",
-//                   borderRadius: 28, // Making the button round
-//                   "&:hover": {
-//                     backgroundColor: "#4752c4", // Slightly darker shade on hover
-//                   },
-//                 }}
-//                 startIcon={
-//                   <img
-//                     src={DiscordIcon}
-//                     alt="Discord"
-//                     style={{ width: 24, height: 24 }}
-//                   />
-//                 }
-//               >
-//                 Join Our Discord
-//               </Button>
-//             </Box>
-//           </Grid>
-//           <Grid
-//             item
-//             xs={12}
-//             md={6}
-//             display="flex"
-//             justifyContent="center"
-//             alignItems="center"
-//           >
-//             <Paper elevation={4} sx={{ borderRadius: 2 }}>
-//               <img
-//                 src={wwuBackground}
-//                 alt="WWU"
-//                 style={{ maxWidth: "100%", borderRadius: "4px" }}
-//               />
-//             </Paper>
-//           </Grid>
-//         </Grid>
-//       </HeroImage>
-
-//       {/* Features Section */}
-//       <Box py={5} textAlign="center">
-//         <Typography variant="h4" marginBottom={5}>
-//           Features
-//         </Typography>
-//         <Grid container spacing={4} justifyContent="center">
-//           <Grid item xs={12} sm={4} textAlign="center">
-//             <FeatureIcon
-//               style={{ backgroundImage: `url(${profileCardIcon})` }}
-//             />
-//             <Typography variant="h6">Profile Cards</Typography>
-//             <Typography>
-//               Learn about fellow alumni, set up meetings, and discover shared
-//               interests.
-//             </Typography>
-//           </Grid>
-//           <Grid item xs={12} sm={4} textAlign="center">
-//             <FeatureIcon
-//               style={{ backgroundImage: `url(${networkingIcon})` }}
-//             />
-//             <Typography variant="h6">Direct Connect</Typography>
-//             <Typography>
-//               Set up meetings through Calendly, and connect in real-time.
-//             </Typography>
-//           </Grid>
-//           <Grid item xs={12} sm={4} textAlign="center">
-//             <FeatureIcon style={{ backgroundImage: `url(${resourcesIcon})` }} />
-//             <Typography variant="h6">Resources</Typography>
-//             <Typography>
-//               Access curated CS resources tailored for our alumni community.
-//             </Typography>
-//           </Grid>
-//         </Grid>
-//       </Box>
-
-//       <Box py={5} textAlign="center">
-//         {/* <Button href='/resources' variant='outlined' color='primary'>
-//           Explore Resources
-//         </Button> */}
-//       </Box>
-//     </Container>
-//   );
-// };
-
 const LandingView = () => {
   return (
-    <Box
-      sx={{
-        width: "100vw",
-        height: "100vh",
-        backgroundImage: `linear-gradient(rgba(19, 129, 255, 0.15), rgba(19, 129, 255, 0.15)), url(${wwuBG})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        overflow: "hidden",
-        position: "relative",
-      }}
-    >
-      <img
-        src="/Vector 6.png"
-        style={{
-          position: "absolute",
-          top: 90,
-          left: 0,
-          bottom: 0,
-          margin: "auto",
+    <Box>
+      <Box
+        sx={{
+          width: "100vw",
+          height: "100vh",
+          backgroundImage: `linear-gradient(rgba(19, 129, 255, 0.15), rgba(19, 129, 255, 0.15)), url(${wwuBG})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          overflow: "hidden",
+          position: "relative",
         }}
-      />
-      <img
-        src="/Vector 4.png"
-        style={{
-          position: "absolute",
-          top: 300,
-          left: 0,
-          bottom: 0,
-          margin: "auto",
-        }}
-      />
-      <img
-        src="/Vector 5.png"
-        style={{
-          position: "absolute",
-          top: 220,
-          left: -50,
-          bottom: 0,
-          margin: "auto",
-        }}
-      />
-      <Stack sx={{ position: "absolute", left: 200 }} spacing={2}>
-        <Stack direction="row">
-          <Typography
-            sx={{
-              color: "white",
-              fontWeight: "bold",
-              fontFamily: "Inconsolata",
-              fontSize: "5.5rem",
-              lineHeight: "normal",
-            }}
-          >
-            {`>`}
-          </Typography>
-          <Stack spacing={2}>
-            <Box>
-              <Typography
+      >
+        <img
+          src="/Vector 6.png"
+          style={{
+            position: "absolute",
+            top: 90,
+            left: 0,
+            bottom: 0,
+            margin: "auto",
+          }}
+        />
+        <img
+          src="/Vector 4.png"
+          style={{
+            position: "absolute",
+            top: 300,
+            left: 0,
+            bottom: 0,
+            margin: "auto",
+          }}
+        />
+        <img
+          src="/Vector 5.png"
+          style={{
+            position: "absolute",
+            top: 220,
+            left: -50,
+            bottom: 0,
+            margin: "auto",
+          }}
+        />
+        <Stack sx={{ position: "absolute", left: 200 }} spacing={2}>
+          <Stack direction="row">
+            <Typography
+              sx={{
+                color: "white",
+                fontWeight: "bold",
+                fontFamily: "Inconsolata",
+                fontSize: "5.5rem",
+                lineHeight: "normal",
+              }}
+            >
+              {`>`}
+            </Typography>
+            <Stack spacing={2}>
+              <Box>
+                <Typography
+                  sx={{
+                    color: "white",
+                    fontWeight: "bold",
+                    fontFamily: "Inconsolata",
+                    fontSize: "5.5rem",
+                    lineHeight: "normal",
+                  }}
+                >
+                  WhatcomCoders
+                </Typography>
+                <Typography
+                  sx={{
+                    color: "white",
+                    lineHeight: "normal",
+                    fontSize: "2rem",
+                  }}
+                >
+                  wwu's network for success in tech
+                </Typography>
+              </Box>
+              <NavButton
                 sx={{
                   color: "white",
-                  fontWeight: "bold",
-                  fontFamily: "Inconsolata",
-                  fontSize: "5.5rem",
-                  lineHeight: "normal",
+                  width: "45%",
+                  justifyContent: "space-between",
+                  fontSize: "1rem",
+                  px: 3,
+                  py: 2.5,
+                  borderRadius: 5,
                 }}
-              >
-                WhatcomCoders
-              </Typography>
-              <Typography
+                text="join our discord"
+                url="/https://discord.gg/r6ShrR73Jx"
+                icon={<FontAwesomeIcon icon={faDiscord} />}
+              />
+              <NavButton
                 sx={{
                   color: "white",
-                  lineHeight: "normal",
-                  fontSize: "2rem",
+                  width: "45%",
+                  justifyContent: "space-between",
+                  fontSize: "1rem",
+                  px: 3,
+                  py: 2.5,
+                  borderRadius: 5,
                 }}
-              >
-                wwu's network for success in tech
-              </Typography>
-            </Box>
-            <NavButton
-              sx={{
-                color: "white",
-                width: "45%",
-                justifyContent: "space-between",
-                fontSize: "1rem",
-                px: 3,
-                py: 2.5,
-                borderRadius: 5,
-              }}
-              text="join our discord"
-              url="/https://discord.gg/r6ShrR73Jx"
-              icon={<FontAwesomeIcon icon={faDiscord} />}
-            />
-            <NavButton
-              sx={{
-                color: "white",
-                width: "45%",
-                justifyContent: "space-between",
-                fontSize: "1rem",
-                px: 3,
-                py: 2.5,
-                borderRadius: 5,
-              }}
-              text="connect with us"
-              url="/people"
-              icon={<FontAwesomeIcon icon={faUser} />}
-            />
-            <NavButton
-              sx={{
-                color: "white",
-                width: "45%",
-                justifyContent: "space-between",
-                fontSize: "1rem",
-                px: 3,
-                py: 2.5,
-                borderRadius: 5,
-              }}
-              text="explore resources"
-              url="/resources"
-              icon={<FontAwesomeIcon icon={faCompass} />}
-            />
-            <NavButton
-              sx={{
-                color: "white",
-                width: "45%",
-                justifyContent: "space-between",
-                fontSize: "1rem",
-                px: 3,
-                py: 2.5,
-                borderRadius: 5,
-              }}
-              text="learn more"
-              url=""
-              icon={<FontAwesomeIcon icon={faAngleDown} />}
-            />
+                text="connect with us"
+                url="/people"
+                icon={<FontAwesomeIcon icon={faUser} />}
+              />
+              <NavButton
+                sx={{
+                  color: "white",
+                  width: "45%",
+                  justifyContent: "space-between",
+                  fontSize: "1rem",
+                  px: 3,
+                  py: 2.5,
+                  borderRadius: 5,
+                }}
+                text="explore resources"
+                url="/resources"
+                icon={<FontAwesomeIcon icon={faCompass} />}
+              />
+              <NavButton
+                sx={{
+                  color: "white",
+                  width: "45%",
+                  justifyContent: "space-between",
+                  fontSize: "1rem",
+                  px: 3,
+                  py: 2.5,
+                  borderRadius: 5,
+                }}
+                text="learn more"
+                url=""
+                icon={<FontAwesomeIcon icon={faAngleDown} />}
+              />
+            </Stack>
           </Stack>
         </Stack>
-      </Stack>
+      </Box>
+      {/* Features Section */}
+      <Box
+        sx={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          scrollSnapAlign: "start",
+          textAlign: "center",
+          py: 5,
+        }}
+      >
+        <Box py={5} textAlign="center">
+          <Typography variant="h4" marginBottom={5}>
+            Features
+          </Typography>
+          <Grid container spacing={4} justifyContent="center">
+            <Grid item xs={12} sm={4} textAlign="center">
+              <FeatureIcon
+                style={{ backgroundImage: `url(${profileCardIcon})` }}
+              />
+              <Typography variant="h6">Profile Cards</Typography>
+              <Typography>
+                Learn about fellow alumni, set up meetings, and discover shared
+                interests.
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={4} textAlign="center">
+              <FeatureIcon
+                style={{ backgroundImage: `url(${networkingIcon})` }}
+              />
+              <Typography variant="h6">Direct Connect</Typography>
+              <Typography>
+                Set up meetings through Calendly, and connect in real-time.
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={4} textAlign="center">
+              <FeatureIcon
+                style={{ backgroundImage: `url(${resourcesIcon})` }}
+              />
+              <Typography variant="h6">Resources</Typography>
+              <Typography>
+                Access curated CS resources tailored for our alumni community.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
     </Box>
   );
 };
